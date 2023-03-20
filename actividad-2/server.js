bd2dde305743a7ff6c94b4122651194b275ca73f
agustin.gonzalez <agustin.gonzalez.10@hotmail.com>
2023-03-19 23:41:21 -0300

    agregando actividad-2

---
+++ actividad-2/server.js
@@ -0,0 +1,52 @@
+// Parte #01
+const http = require('node:http')
+
+const server = http.createServer(function(request, response) {
+
+    console.log("escuchando en el puerto 3002");
+
+    if (request.url == "/") {
+        response.write("Agustin Gonzalez")
+    } else if (request.url == "/productos") {
+        let productos = [
+            {
+                id: 1,
+                nombre: "Café Expreso",
+                precio: 200
+            },
+            {
+                id: 2,
+                nombre: "Café Americano",
+                precio: 250
+            },
+            {
+                id: 3,
+                nombre: "Café Cortado",
+                precio: 200
+            },
+            {
+                id: 4,
+                nombre: "Café Doble",
+                precio: 250
+            },
+            {
+                id: 4,
+                nombre: "Café Lagrima",
+                precio: 200
+            }
+        ]
+    } else if (request.url == "/materia") {
+        response.write("Materia: Aplicaciones Hibridas")
+    } else if (request.url == "/profesor") {
+        response.write("Profesor: Brian Lara")
+    } else {
+        response.write("Pagina no encontrada")
+    }
+
+    // Parte #03
+    
+    response.end()
+    
+})
+
+server.listen(3002)
